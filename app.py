59bceaab1ef113132635e3734ec79e26e3c228d6
yashvasudeva1 <117675995+yashvasudeva1@users.noreply.github.com>
2025-07-18 23:03:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Set page configuration
 st.set_page_config(page_title="Polynomial Regression Predictor", layout="wide")
